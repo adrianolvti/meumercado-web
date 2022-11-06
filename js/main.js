391a87d0fd0b2a6e21685d82c30d15286da118d6
Adriano Lucas Vieira <adrianolvti@hotmail.com>
2022-11-06 13:11:11 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function allProducts(url) {
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send();
+    return request.responseText;
+}
+
+function newLineTable(product) {
+    let line = document.createElement("tr");
+    let tdCode = document.createElement("td");
+    let tdName = document.createElement("td");
+    let tdType = document.createElement("td");
+    let tdPrice = document.createElement("td");
+    let tdInventory = document.createElement("td");
+
+    tdCode.innerHTML = product.code;
+    tdName.innerHTML = product.name;
+    tdType.innerHTML = product.type.name;
+    tdPrice.innerHTML = product.price;
+    tdInventory.innerHTML = product.inventory;
+
+    line.appendChild(tdCode);
+    line.appendChild(tdName);
+    line.appendChild(tdType);
+    line.appendChild(tdPrice);
+    line.appendChild(tdInventory);
+
+    return line;
+}
+
+function main() {
+    let data = allProducts("http://localhost:8080/product");
+    let products = JSON.parse(data);
+    let table = document.getElementById("table");
+
+    products.forEach(product => {
+        let line = newLineTable(product);
+        table.appendChild(line);
+    });
+}
+
+main()
